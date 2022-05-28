37b19ab20720150eeae0f594c742e289a6164c85
antonious-gergis <antoniousgerges65@gmail.com>
2022-05-28 16:45:16 +0200

    Finished section 08 Client - Angular Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'E-Commerce';
+  products:IProduct[] | undefined;
+  pagination:IPagination | undefined;
+  
+  constructor(private http:HttpClient ){
+
+  }
+
+  ngOnInit(): void {
+
+
+    this.http.get('https://localhost:44389/api/Products/GetAllProducts').subscribe(
+      (response:any):void => {
+      this.products = response.data;    
+
+    }, error =>{
+      console.log(error)
+    });
+    
+  }
 }
